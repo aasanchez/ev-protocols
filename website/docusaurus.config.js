6574ac5c2a9cd48efe22e03bdf79506b92df539e
Alexis Sanchez <alexis.sanchez@eva.global>
2023-08-01 14:08:48 +0200

    ✨ feat: Add sitemap configuration to Docusaurus
    Add sitemap configuration to `docusaurus.config.js` and install `@docusaurus/plugin-sitemap` in `package.json`.
    
    Changes in `docusaurus.config.js`:
    - Added `sitemap` object with the following properties:
      - `changefreq: 'weekly'`
      - `priority: 0.5`
      - `ignorePatterns: ['/tags/**']`
      - `filename: 'sitemap.xml'`
    
    Changes in `package.json`:
    - Added `"@docusaurus/plugin-sitemap": "^2.4.1"` to dependencies.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -58,7 +58,13 @@ const config = {
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css')
-        }
+        },
+        sitemap: {
+          changefreq: 'weekly',
+          priority: 0.5,
+          ignorePatterns: ['/tags/**'],
+          filename: 'sitemap.xml',
+        },
       })
     ]
   ],
